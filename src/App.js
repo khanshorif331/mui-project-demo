036731aaa222a620d71b7cb24d44edaca174db28
khanshorif331 <khanshorif331@gmail.com>
2022-07-09 01:04:13 +0600

    initial setup dooone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Typography } from '@mui/material'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Typography variant='h1'> Hello world</Typography>
+		</div>
+	)
 }
 
-export default App;
+export default App
